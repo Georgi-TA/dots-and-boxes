a5df49cb804e3c8273636d7bc8a884ad3b310c97
Georgi Chakarov <geochak@gmail.com>
2017-03-08 16:17:59 +0200

    Adding a launcher icon and partially implemented Mutiplayer support.

---
+++ app/src/main/java/info/scelus/dotsandboxes/gametree/Dots.java
@@ -0,0 +1,9 @@
+package info.scelus.dotsandboxes.gametree;
+
+/**
+ * Created by scelus on 08.03.17
+ */
+
+class Dots {
+
+}
---
+++ app/src/main/java/info/scelus/dotsandboxes/gametree/GameTree.java
@@ -0,0 +1,20 @@
+package info.scelus.dotsandboxes.gametree;
+
+import java.util.ArrayList;
+
+/**
+ * Created by scelus on 08.03.17.
+ */
+
+public class GameTree {
+
+
+
+    public ArrayList<Dots> dots;
+    public HashMap<Moves> moves;
+
+    public GameTree() {
+
+    }
+
+}
