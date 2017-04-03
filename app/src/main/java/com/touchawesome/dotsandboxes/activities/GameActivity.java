f16c99c5310f0ff8ca5d11a59c45d99ea939b72b
Georgi Chakarov <geochak@gmail.com>
2017-04-03 10:58:46 +0300

    Minor UI/UX changes. Delayed the bot move with a quarter second.

--- app/src/main/java/com/touchawesome/dotsandboxes/activities/GameActivity.java
+++ app/src/main/java/com/touchawesome/dotsandboxes/activities/GameActivity.java
@@ -1,11 +1,9 @@
 package com.touchawesome.dotsandboxes.activities;
 
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
